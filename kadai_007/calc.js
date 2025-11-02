5cd021eeb9a5abd5d0c426632a63ff7410fd10ca
meguro0525se-bit <meguro.0525se@gmail.com>
2025-11-02 16:41:12 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,8 +1,6 @@
 let num = Math.floor(Math.random() * 100)+1;
 
-console.log(num);
-
-if (num % 3 === 0 && num % 5 === 0) {
+if (num % 15 === 0) {
   console.log('3と5の倍数です');
 }
 
@@ -14,5 +12,8 @@ else if (num % 5 == 0) {
   console.log('5の倍数です');
 }
 
-else{}
+else{
+  console.log(num);
+}
+
 
